b26f3f5716933d1196fabdb1b56d3ed691c2c1ba
Bruno Chikuji <bruno-chikuji@hotmail.com>
2020-01-10 17:08:22 -0300

    Teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Downloader } from '@ionic-native/downloader';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FileTransfer }     from '@ionic-native/file-transfer';
+import { File }                                 from '@ionic-native/file';
+import { PhotoLibrary }                         from '@ionic-native/photo-library';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    FileTransfer,
+    PhotoLibrary,
+    Downloader,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FileTransfer, FileTransferObject }     from '@ionic-native/file-transfer';
+import { File }                                 from '@ionic-native/file';
+import { PhotoLibrary }                         from '@ionic-native/photo-library';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private file: File,
+    public navCtrl: NavController,
+    private photoLibrary: PhotoLibrary,
+    private transfer: FileTransfer
+    ) { }
 
-  }
+    downloadPhoto() {
+      console.log('veio');
+      let urlphoto: string = 'https://lh3.googleusercontent.com/NNyZEbSe0sEmsaRCQRPpJsHqOQo40_a9_L3Kwsy3kQb2jR96B0Pnf3fjWd3pyiVhExtgAYBR9BjVQ5VpFHpFbeeblumc629HX2q-OAf_IA';
+      this.photoLibrary.requestAuthorization().then(() => {
+        let url: string = encodeURI(urlphoto);
+        let fileName: string = `${new Date().getTime().toString()}.jpg`;
+        let fileTransfer: FileTransferObject = this.transfer.create();
+        let filePath: string = `${this.file.dataDirectory}Download/${fileName}`;
+        fileTransfer.download(url, filePath, true).then(
+            teste => {
+              alert('Baixou a imagem, olhe nos documentos');
+              console.log(JSON.stringify(teste));
+            }
+          ).catch(error => {
+              alert('Deu erro');
+              console.log(JSON.stringify(error));
+          });
+
+      });
+
+      }
 
 }
